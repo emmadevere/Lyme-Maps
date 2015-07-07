be2be3c37b5a475021a39b214b142827d2fc9bb6
emmadevere <emmadevere@gmail.com>
2015-07-07 02:23:43 -0400

    Fixed Utah bug

--- LymeMaps.R
+++ LymeMaps.R
@@ -17,12 +17,22 @@ my.dat$change <- (my.dat$X2013.Confirmed - my.dat$X2004)
 
 #MAPPING THE DIFFERENCE
 #THE BREAKPOINTS BETWEEN COLORS
-my.dat$colorfill <- as.factor(as.numeric(cut(my.dat$change, 
+my.dat$colorfill <- as.numeric(cut(my.dat$change, 
     c(-2000, -1500, -1000, -500, 0, 500, 1000, 1500,
-      2000, 2500))))
+      2000, 2500)))
+#colorfill needs to be a factor for matching data to maps but
+#if done in the above, takes absolute values of my.dat$change
+my.dat$colorfill <- as.factor(my.dat$colorfill)
+
 #REPRESENTATIONAL COLORS
-palette = c("#d53e4f", "#f46d43",  "#fdae61", "#fee08b", 
-           "#d53e4f", "#f46d43", "#fdae61", "#fee08b")
+
+###palette = c("#4575b4", "#74add1",  "#abd9e9", "#e0f3f8", 
+###"#ffffbf", "#fee090", "#fdae61", "#f46d43", "#d73027")
+#ABOVE ARE COLORS FOR EACH LEVEL, BELOW IS WHAT 
+#COLORS/LEVELS EXIST FOR THIS SPECIFIC MAP
+
+palette = c("#4575b4", "#e0f3f8", 
+           "#ffffbf", "#fee090", "#fdae61", "#d73027")
 #MATCHING STATE TO MAP DATA
 mapnames <- map("state" ,plot=FALSE)[4]$names
 colorsmatched <- my.dat$colorfill [na.omit(match(mapnames,
@@ -40,11 +50,17 @@ my.dat$mean1213 <- apply(subset(my.dat,
     select = c(X2012, X2013.Confirmed)), 1, mean)
 my.dat$change2 <- (my.dat$mean1213 - my.dat$mean0405)
 
+
+
 #MAPPING
-my.dat$colorfill2 <- as.factor(as.numeric(cut(my.dat$change2, 
-    c(-10000, -500, 0, 500, 10000))))
+my.dat$colorfill2 <- as.numeric(cut(my.dat$change2, 
+    c(-2600, -1500, -1000, -500, 0, 500, 1000, 1500,
+      2000, 2500)))
+my.dat$colorfill2 <- as.factor(my.dat$colorfill2)
 colorsmatched <- my.dat$colorfill2 [na.omit(match(mapnames,
    my.dat$State))]
+palette = c("#4575b4",  "#e0f3f8", 
+            "#ffffbf", "#fee090", "#f46d43")
 map("state", col = palette[my.dat$colorfill2], fill = TRUE, 
     resolution = 0, lty = 1, lwd= 0.2, bg = "gray")
 title(main="Change in Lyme Disease Incidence (2004-2013)",cex.main=1.2)
