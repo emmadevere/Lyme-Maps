00dd10d4e9aa050c44b8a2a6875e44bf6d4453ce
emmadevere <emmadevere@gmail.com>
2015-07-09 00:45:04 -0400

    1st commit

---
+++ MeanMap.R
@@ -0,0 +1,34 @@
+library(maps)
+library(RColorBrewer)
+
+setwd("~/GitHub/Lyme-Maps")
+my.dat <- read.csv("lymestates.csv", header = TRUE, sep = ";")
+
+#CONVERTING 1ST COLUMN INTO ROW NAMES 
+rownames(my.dat) <- my.dat[,1]
+my.dat[,1] <- NULL
+
+#CALCULATING MEAN OCCURANCE FROM 2004-2013 FOR EACH STATE
+my.dat$mean <- apply(my.dat, 1, mean)
+
+#MAP
+#THE BREAKPOINTS BETWEEN COLORS
+my.dat$colorfill <- as.numeric(cut(my.dat$mean, 
+    c(0, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500)))
+#colorfill needs to be a factor for matching data to maps but
+#if done in the above, takes absolute values of my.dat$change
+my.dat$colorfill <- as.factor(my.dat$colorfill)
+
+palette = c("#fff7ec", "#fee8c8",  "#fdd49e", "#fdbb84", 
+    "#fc8d59", "#ef6548", "#d7301f", "#7f0000")
+
+#MATCHING STATE TO MAP DATA
+my.dat$combo <- paste(rownames(my.dat), sep=",")
+mapnames <- map("state" ,plot=FALSE)[4]$names
+colorsmatched <- my.dat$colorfill [na.omit(match(mapnames,
+    my.dat$combo))]
+
+#MAPPING
+map("state", col = palette[my.dat$colorfill], fill = TRUE, 
+    resolution = 0, lty = 1, lwd= 0.2, bg = "gray")
+title(main="Change in Lyme Disease Incidence (2004-2013)",cex.main=1.2)
